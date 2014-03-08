34fef6e442c1aad44d5d3eec6a3bcfb88f0638a6
abarre <anthony.barre87@gmail.com>
2014-03-08 18:42:53 +0100

    Update ngx_pagespeed to 1.7.30.3

--- site-cookbooks/nginx_with_pagespeed/attributes/ngx_pagespeed.rb
+++ site-cookbooks/nginx_with_pagespeed/attributes/ngx_pagespeed.rb
@@ -22,13 +22,14 @@
 default['nginx']['version'] = '1.4.4'
 default['nginx']['source']['modules'] = node['nginx']['source']['modules'] | ["nginx_with_pagespeed"]
 
-default['nginx']['ngx_pagespeed']['version']  = "1.7.30.1-beta"
+default['nginx']['ngx_pagespeed']['version']  = "1.7.30.3-beta"
 default['nginx']['ngx_pagespeed']['url']      = "https://github.com/pagespeed/ngx_pagespeed/archive/v#{node['nginx']['ngx_pagespeed']['version']}.tar.gz"
 default['nginx']['ngx_pagespeed']['src']['cookbook'] = "nginx"
 default['nginx']['ngx_pagespeed']['src']['file'] = nil
 
-default['nginx']['ngx_pagespeed']['psol']['version']  = "1.7.30.1"
+default['nginx']['ngx_pagespeed']['psol']['version']  = "1.7.30.3"
 default['nginx']['ngx_pagespeed']['psol']['url'] = "https://dl.google.com/dl/page-speed/psol/#{node['nginx']['ngx_pagespeed']['psol']['version']}.tar.gz"
+#default['nginx']['ngx_pagespeed']['psol']['url'] = "http://mirror.nodesdirect.com/internal/cust/#{node['nginx']['ngx_pagespeed']['psol']['version']}.tar.gz"
 default['nginx']['ngx_pagespeed']['psol']['src']['cookbook'] = "nginx"
 default['nginx']['ngx_pagespeed']['psol']['src']['file'] = nil
 

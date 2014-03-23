394378a42055ad1515cb9f195321ad18b8923285
abarre <anthony.barre87@gmail.com>
2014-03-23 13:30:39 +0100

    Improve the cache of varnish.

--- site-cookbooks/varnish_for_passenger/attributes/default.rb
+++ site-cookbooks/varnish_for_passenger/attributes/default.rb
@@ -1,3 +1,4 @@
 override['varnish']['vcl_cookbook'] = "varnish_for_passenger"
 default['varnish']['storage_size'] = '500M'
 default['varnish']['listen_port'] = 80
+override['varnish']['instance'] = "lovelycarte.com"

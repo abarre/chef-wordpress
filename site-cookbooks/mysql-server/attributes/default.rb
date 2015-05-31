4af350aee9a8fee50192df7fc97375246682ea2b
abarre <anthony.barre87@gmail.com>
2015-05-31 17:09:59 +0200

    Update mysql settings

---
+++ site-cookbooks/mysql-server/attributes/default.rb
@@ -0,0 +1,3 @@
+override['mysqld']['my.cnf']['mysqld']['innodb_log_buffer_size'] = '1G'
+override['mysqld']['my.cnf']['mysqldump']['max_allowed_packet'] = '64M'
+override['mysqld']['my.cnf']['mysqld']['max_allowed_packet'] = '64M'
